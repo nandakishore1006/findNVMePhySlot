4088bb05ee898ea5f7140822b2d76526f56f92fb
nandakishore1006 <kishore1006@gmail.com>
2020-08-11 01:30:07 +0530

    Update findNVMePhysicalSlotWithoutNVMeCLI.sh

--- findNVMePhysicalSlotWithoutNVMeCLI.sh
+++ findNVMePhysicalSlotWithoutNVMeCLI.sh
@@ -5,15 +5,13 @@ for i in $(ls /sys/block/| grep -i nvme)
 do
 	deviceLink=$(readlink -f /sys/block/$i)
 	if [[ $deviceLink == *"virtual"* ]]; then
-	continue 
+		continue 
 	
- fi
- serial=$(cat /sys/block/$i/device/serial)	
- controller=$(basename $(dirname $deviceLink))
- bdf=$(basename $(dirname $(dirname $(dirname $deviceLink))))
- physicalSlot=$(lspci -v -s $bdf| awk '/Physical Slot:/{print $3}')
- if [[ ]]; then
-	 i=
- echo "Physical Slot for the Block Device \"$i\" with Serial \"$serial\" under NVMe Contoller-\"$controller\" = \"$physicalSlot\""
+ 	fi
+ 	serial=$(cat /sys/block/$i/device/serial)	
+ 	controller=$(basename $(dirname $deviceLink))
+ 	bdf=$(basename $(dirname $(dirname $(dirname $deviceLink))))
+ 	physicalSlot=$(lspci -v -s $bdf| awk '/Physical Slot:/{print $3}')
+ 	echo "Physical Slot for the Block Device \"$i\" with Serial \"$serial\" under NVMe Contoller-\"$controller\" = \"$physicalSlot\""
 
 done
